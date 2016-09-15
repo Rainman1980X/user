b169e879615578e1f009eeb8500691ba929d6cf1
Matthias Burger <matthias.burger@sintec.de>
2016-09-15 10:00:33 +0200

    Start des CompanyController Anpassung der GetAllUser Action

--- src/src/main/java/s3f/ka_user_store/controllers/CompanyController.java
+++ src/src/main/java/s3f/ka_user_store/controllers/CompanyController.java
@@ -1,7 +1,22 @@
 package s3f.ka_user_store.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.RestController;
+import s3f.ka_user_store.interfaces.UserRepository;
+
 /**
  * Created by MSBurger on 09.09.2016.
  */
+@RestController
 public class CompanyController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CompanyController.class);
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 }
--- src/src/main/java/s3f/ka_user_store/services/User/GetAllUserAction.java
+++ src/src/main/java/s3f/ka_user_store/services/User/GetAllUserAction.java
@@ -24,8 +24,10 @@ public class GetAllUserAction implements UserActions<Map<String,String>> {
                                                         String authorization,
                                                         String correlationToken,
                                                         Map<String,String> httpValues) {
+        LOGGER.info("Get all User");
         List<UserDto> userList = userRepository.findAll();
         if(userList.isEmpty()){
+            LOGGER.info("Now user found");
             return new ResponseEntity<List<UserDto>>(new ArrayList<UserDto>(), HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<List<UserDto>>(userList,HttpStatus.OK);
